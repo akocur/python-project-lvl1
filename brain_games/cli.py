b9276a689a185e94bfe35f3a4c2aebd95e6aed1a
akocur <akocur@yandex.ru>
2021-05-10 21:52:57 +0300

    add the return value in welcome_user()

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -3,6 +3,11 @@ import prompt
 
 
 def welcome_user():
-    """Asks for a name and greets."""
+    """Asks for a name and greets.
+
+    Returns:
+        string: user name
+    """
     name = prompt.string('May I have your name? ')
     print('Hello, {0}!'.format(name))  # noqa: WPS421
+    return name
