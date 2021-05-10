c4e928a371d51c19bc56d29c56750b445ce8a05b
akocur <akocur@yandex.ru>
2021-05-10 21:55:26 +0300

    add global variable user_name

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,11 +2,14 @@
 """Brain games."""
 from brain_games.cli import welcome_user
 
+user_name = None
+
 
 def main():
     """Entry point."""
     print('Welcome to the Brain Games!')
-    welcome_user()
+    global user_name
+    user_name = welcome_user()
 
 
 if __name__ == '__main__':
