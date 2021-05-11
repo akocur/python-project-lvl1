4c2c2ba622790e289d6c8972d131b7e971acb19f
akocur <akocur@yandex.ru>
2021-05-11 15:46:09 +0300

    add run() to engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,6 +1,26 @@
 import prompt
 
 
+def run(game_module):
+    """
+    Run game
+    :param game_module:
+        Game module.
+    :return:
+    """
+    user_name = game_module.welcome_user()
+    question_count = 3
+    for _ in range(question_count):
+        question = game_module.ask_question()
+        answer = get_answer()
+        if not is_correct_answer(game_module, question, answer):
+            game_over(user_name, answer, game_module.get_correct_answer(question))
+            break
+        print('Correct!')
+    else:
+        congratulations(user_name)
+
+
 def get_answer():
     """Get user answer.
 
