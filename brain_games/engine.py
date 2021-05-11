cd83c6627110bfe8c03cea4186f43296686a4d72
akocur <akocur@yandex.ru>
2021-05-11 15:34:01 +0300

    add is_correct_answer() in engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -25,3 +25,21 @@ def game_over(user_name, answer, correct_answer):
     print("'{0}' is wrong answer ;(. Correct answer was '{1}'."
           .format(answer, correct_answer))
     print("Let's try again, {0}!".format(user_name))
+
+
+def is_correct_answer(game_module, question, answer):
+    """Check if the answer is correct.
+
+    :param game_module:
+        Game module.
+    :param int question:
+        Any integer number. See ask_question() function.
+    :param str answer:
+        User answer. See get_answer() function.
+    :returns:
+        True if answer is correct else False
+    :rtype:
+        bool
+    """
+    correct_answer = game_module.get_correct_answer(question)
+    return correct_answer == answer
