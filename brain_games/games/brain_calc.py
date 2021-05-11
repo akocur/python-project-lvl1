68482fc6fbd9e922e1b0c6b617c39b3d348ef2f0
akocur <akocur@yandex.ru>
2021-05-11 17:39:07 +0300

    add ask_question() to games/brain_calc.py

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,2 +1,16 @@
+from random import randint
+from random import choice
+
+
 def show_game_rules():
     print("What is the result of the expression?")
+
+
+def ask_question():
+    number_one = randint(0, 100)
+    number_two = randint(0, 100)
+    available_operation = '+-*'
+    operation = choice(available_operation)
+    expression = '{0} {1} {2}'.format(number_one, operation, number_two)
+    print('Question: ', expression)
+    return number_one, operation, number_two
