f6637123486b4baee043675369630f3dd3ea3308
akocur <akocur@yandex.ru>
2021-05-11 16:44:57 +0300

    add get_correct_answer() to brain_games/games/brain_even.py

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -1,3 +1,19 @@
+answer_yes = 'yes'
+answer_no = 'no'
+
+
+def get_correct_answer(question):
+    """Get correct answer.
+
+    :returns:
+        "yes" if question is even number else "no"
+    :rtype:
+        str
+    """
+    global answer_yes, answer_no
+    return answer_yes if is_even_number(question) else answer_no
+
+
 def is_even_number(number):
     """Check if number is even.
 
