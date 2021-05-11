95776e6e7fd295b725fb541a87164bbd6f9ad092
akocur <akocur@yandex.ru>
2021-05-11 16:03:14 +0300

    add brain_games/scripts/brain_calc.py

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,14 @@
+#!/usr/env python3
+"""Brain calculation."""
+
+import brain_games.games.brain_calc as brain_calc
+import brain_games.engine as engine
+
+
+def main():
+    """Entry point."""
+    engine.run(brain_calc)
+
+
+if __name__ == '__main__':
+    main()
