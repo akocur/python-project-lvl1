8248be5d90c68be7fa207d904c25b4d3cf6ac74e
akocur <akocur@yandex.ru>
2021-05-11 16:41:58 +0300

    add is_even_number() to brain_games/games/brain_even.py

---
+++ brain_games/games/brain_even.py
@@ -0,0 +1,11 @@
+def is_even_number(number):
+    """Check if number is even.
+
+    :param int number:
+        Any number.
+    :returns:
+        True if number is even else False.
+    :rtype:
+        bool
+    """
+    return number % 2 == 0
