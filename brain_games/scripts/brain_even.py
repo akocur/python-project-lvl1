44118c19176bb7550c3922af17a4436ba1821c44
akocur <akocur@yandex.ru>
2021-05-11 16:38:47 +0300

    modify main() in brain_games/script/brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,32 +1,13 @@
+#!/usr/env python3
 """Brain even."""
 
-from random import randint
-
-import prompt
-from brain_games.cli import welcome_user
-
-answer_yes = 'yes'
-answer_no = 'no'
+import brain_games.games.brain_even as brain_even
+import brain_games.engine as engine
 
 
 def main():
     """Entry point."""
-    user_name = welcome_user()
-    global answer_yes, answer_no
-    print('Answer "{0}" if the number is even, otherwise answer "{1}".'
-          .format(answer_yes, answer_no))
-    question_count = 3
-    for _ in range(question_count):
-        question = ask_question()
-        answer = get_answer()
-        if not is_correct_answer(question, answer):
-            game_over(user_name, answer, get_correct_answer(question))
-            break
-        print('Correct!')
-    else:
-        congratulations(user_name)
-
-
+    engine.run(brain_even)
 
 
 if __name__ == '__main__':
