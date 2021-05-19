1ee4322c3b06b5d5ddb7483bf847612228fa78c6
akocur <akocur@yandex.ru>
2021-05-19 20:07:24 +0300

    add scripts/brain_gcd.py. Implement main() function.

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,14 @@
+#!/usr/env python3
+"""Brain game greatest common divisor."""
+
+import brain_games.games.brain_gcd as brain_gcd
+import brain_games.engine as engine
+
+
+def main():
+    """Entry point."""
+    engine.run(brain_gcd)
+
+
+if __name__ == '__main__':
+    main()
