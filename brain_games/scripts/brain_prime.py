33e1402751157ded80d0e9b13da793f478715495
akocur <akocur@yandex.ru>
2021-05-22 19:46:38 +0300

    implement main() in games/brain_prime.py

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,14 @@
+#!/usr/env python3
+"""Brain game prime number."""
+
+import brain_games.games.brain_prime as brain_prime
+import brain_games.engine as engine
+
+
+def main():
+    """Entry point."""
+    engine.run(brain_prime)
+
+
+if __name__ == '__main__':
+    main()
