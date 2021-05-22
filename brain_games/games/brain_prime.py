3a2f4dbd4701ee7bcd8db8736eda7920db8d0eb1
akocur <akocur@yandex.ru>
2021-05-22 20:14:32 +0300

    implement ask_question() in games/brain_prime.py

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -1,3 +1,19 @@
+from random import randint
+
+
 def show_game_rules():
     """Show game rules."""
     print('Answer "yes" if given number is prime. Otherwise answer "no".')
+
+
+def ask_question():
+    """Ask question.
+
+    :returns:
+        number
+    :rtype
+        int
+    """
+    number = randint(0, 100)
+    print('Question: ', number)
+    return number
